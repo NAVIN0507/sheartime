e077ba15a1f36aaffd06c7dfcd32ca2e8b4fc805
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-15 11:32:43 +0530

    Added Hero Card bu not completed

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
 
 const HeroCard = () => {
   return (
   <section className='hero-card'>
-    <h1 className='text-white text-4xl'>Hi There 👋</h1>
+    <div className='flex flex-col'>
+    <h1 className='text-white '>Hi There 👋</h1>
+    <h3 className=' mt-2 text-white'>Welcome to ShearTime</h3>
+    <p className='text-white text-4xl'>Get the Perfect Cut, Every Time <br />
+    Book Your Appointment in Just a Few Clicks</p>
+    <div>
+        <Button  className='main-btn' asChild>
+            <Link href="/sign-in" className='main-btn'>
+            Create Your First Booking
+            </Link>
+        </Button>
+    </div>
+    </div>
   </section>
   )
 }
