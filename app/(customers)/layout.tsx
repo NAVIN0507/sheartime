1c9fd8eabf17852298ed542230f7ec9557bf2158
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-18 14:14:28 +0530

    modified Navigation

--- app/(customers)/layout.tsx
+++ app/(customers)/layout.tsx
@@ -1,16 +1,29 @@
 import { auth } from '@/auth'
 import Navbar from '@/components/customers/Navbar'
 import React, { ReactNode } from 'react'
+import localFont from "next/font/local"
+import { Toaster } from "@/components/ui/toaster"
+import { Inter } from 'next/font/google'
+import { redirect } from 'next/navigation'
 
+const fontInter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"]
+  
+})
 const layout = async ({children} :{children: ReactNode}) => {
     const session = await auth();
-    if(!session) return null;
+    if(!session) return redirect("/sign-in");
   return (
-<div className='flex flex-col gap-2'>
-    <Navbar session={session}/>
+    <main className={`${fontInter.className} antialiased`}>
+        <div className='flex flex-col gap-2'>
+            <Navbar session={session}/> 
+        {children}
+        <Toaster />
 
-    {children}
-</div>
+        </div>
+        </main>
 
     )
 }
--- components/customers/Navbar.tsx
+++ components/customers/Navbar.tsx
@@ -2,8 +2,14 @@ import { Session } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Avatar, AvatarFallback } from '../ui/avatar'
+import { getIntials } from '@/lib/utils'
+import { signOut } from '@/auth'
+import { Button } from '../ui/button'
+import { redirect } from 'next/navigation'
 
 const Navbar = ({session}:{session:Session}) => {
+    if(!session) return redirect("/sign-in")
   return (
     <header className='top-0 flex justify-between gap-5 '>
         <Link href="/" className='flex flex-row gap-1'>
@@ -14,11 +20,23 @@ const Navbar = ({session}:{session:Session}) => {
             width={50}
     height={40}
     />
-    <h1 className='font-bold mt-2 text-3xl ml-2 text-green-1'>ShearTime</h1>
+    <h1 className='font-bold mt-2 text-3xl ml-2 text-green-1'>SHEARTIME</h1>
         
         </Link>
         <ul className='flex flex-row items-center gap-8'>
-            <li></li>
+            <li>
+                <Link href={`/my-profile/${session?.user?.id}`}>
+                <Avatar>
+                    <AvatarFallback className='bg-amber-50'>{getIntials(session?.user?.name)}</AvatarFallback>
+                </Avatar>
+                </Link>
+            </li>
+            <li>
+            <form action={async()=>{
+                "use server"
+                await signOut();
+            }}><Button>Logout</Button></form>
+            </li>
         </ul>
     </header>
   )
