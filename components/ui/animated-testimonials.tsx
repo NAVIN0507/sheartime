49ec9a1bafe3ebfedb4653511f8b87ffbe62e04f
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-22 11:32:23 +0530

    Modified Testimonials section

--- components/ui/animated-testimonials.tsx
+++ components/ui/animated-testimonials.tsx
@@ -43,7 +43,7 @@ export const AnimatedTestimonials = ({
     return Math.floor(Math.random() * 21) - 10;
   };
   return (
-    <div className="max-w-sm md:max-w-4xl mx-auto antialiased rounded-3xl mt-10 bg-primary-2 font-sans px-4 md:px-8 lg:px-12 py-20">
+    <div className="max-w-sm md:max-w-4xl mx-auto antialiased rounded-3xl mt-10 bg-primary-1 shadow-2xl font-sans px-4 md:px-8 lg:px-12 py-20">
       <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
         <div>
           <div className="relative h-80 w-full">
@@ -68,7 +68,7 @@ export const AnimatedTestimonials = ({
                     y: isActive(index) ? [0, -80, 0] : 0,
                   }}
                   exit={{
-                    opacity: 0,
+                    opacity: 1,
                     scale: 0.9,
                     z: 100,
                     rotate: randomRotateY(),
@@ -105,20 +105,20 @@ export const AnimatedTestimonials = ({
             }}
             exit={{
               y: -20,
-              opacity: 0,
+              opacity: 1,
             }}
             transition={{
               duration: 0.2,
               ease: "easeInOut",
             }}
           >
-            <h3 className="text-2xl font-bold dark:text-white text-white">
+            <h3 className="text-2xl font-bold dark:text-white text-black">
               {testimonials[active].name}
             </h3>
-            <p className="text-sm text-white  dark:text-neutral-500">
+            <p className="text-sm text-black  dark:text-neutral-500">
               {testimonials[active].designation}
             </p>
-            <motion.p className="text-lg text-white mt-8 dark:text-neutral-300">
+            <motion.p className="text-lg text-black mt-8 dark:text-neutral-300">
               {testimonials[active].quote.split(" ").map((word, index) => (
                 <motion.span
                   key={index}
@@ -147,13 +147,13 @@ export const AnimatedTestimonials = ({
           <div className="flex gap-4 pt-12 md:pt-0">
             <button
               onClick={handlePrev}
-              className="h-7 w-7 rounded-full bg-primary-2 dark:bg-neutral-800 flex items-center justify-center group/button"
+              className="h-7 w-7 rounded-full bg-primary-1 shadow-lg dark:bg-neutral-800 flex items-center justify-center group/button"
             >
               <IconArrowLeft className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:rotate-12 transition-transform duration-300" />
             </button>
             <button
               onClick={handleNext}
-              className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button"
+              className="h-7 w-7 rounded-full bg-primary-1 shadow-md dark:bg-neutral-800 flex items-center justify-center group/button"
             >
               <IconArrowRight className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:-rotate-12 transition-transform duration-300" />
             </button>
