ea3ffc76b30a88ff2fb8a6a326c48ac62b5b64bf
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-23 00:32:29 +0530

    Modified customers users page

--- components/customers/Navbar.tsx
+++ components/customers/Navbar.tsx
@@ -11,40 +11,45 @@ import { redirect } from 'next/navigation'
 const Navbar = ({session}:{session:Session}) => {
     if(!session) return redirect("/sign-in")
   return (
-    <header className='top-0 flex justify-between gap-5 '>
+    <header className='z-10 w-full p-3 top-0 bg-primary-1 shadow-md'>
+        <div className='flex flex-row justify-between'>
+            <div className='flex flex-col gap-1'>
         <Link href="/" className='flex flex-row gap-1'>
       
             <Image
-            src="/icons/logo-main.svg"
+            src="/icons/logo-white.svg"
             alt='logo'
             width={50}
     height={40}
     />
-    <h1 className='font-bold mt-2 text-3xl ml-2 text-green-1'>SHEARTIME</h1>
+    <h1 className='font-bold mt-2 text-3xl ml-2 text-black'>SHEARTIME</h1>
         
         </Link>
-        
+        </div>
+        <div className='flex flex-col items-end justify-end my-auto'>
         <ul className='flex flex-row items-center gap-8 mt-2'>
             
             <li>
             
                 <Link href={`/my-profile/${session?.user?.id}`}>
                 
-                <Avatar>
-                    <AvatarFallback className='bg-amber-50'>{
+                <Avatar className=''>
+                    <AvatarFallback className='bg-cyan-400'>{
                     //@ts-ignore
                     getIntials(session?.user?.name)
                     }</AvatarFallback>
                 </Avatar>
                 </Link>
             </li>
-            <li>
+            <li className='mr-15'> 
             <form action={async()=>{
                 "use server"
                 await signOut();
-            }}><Button className='bg-red-300 text-black'>Logout</Button></form>
+            }}><Button className='bg-secondry-1 text-primary-1 shadow-none'>Logout</Button></form>
             </li>
         </ul>
+        </div>
+        </div>
     </header>
   )
 }
