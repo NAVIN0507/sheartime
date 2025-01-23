7c7824d4cb7ecb00669b9941978ee4b3fdf0ddd8
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-23 23:44:40 +0530

    Modified some routing

--- app/(admin)/admin/onboarding/[id]/page.tsx
+++ app/(admin)/admin/onboarding/[id]/page.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button'
+import { db } from '@/database/drizzle'
+import { users } from '@/database/schema'
+import { getUserById } from '@/lib/actions/user.action'
+import { eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
+import React, { useState }  from 'react'
 
-const page = () => {
+const page = ({params} :{params:{id:string}}) => {
+const [isLoading, setisLoading] = useState(false)
+  const onSubmit = async () => {
+  setisLoading(true)
+    // TODO: Implement the onboarding logic
+    console.log('Onboarding logic for user with id:', params.id)
+    try {
+      const user = await db.update(users).set({onBoarded: true}).where(eq(users.id , params.id));
+      const getuser = await getUserById(params.id);
+      if(!getuser?.onBoarded) {
+        return redirect(`/admin/${params.id}`)
+      }
+       setisLoading(false)
+
+    } catch (error) {
+      console.log(error)
+      setisLoading(false)
+    }
+  }
   return (
-    <div>Onboarding Page</div>
+    <div>Onboarding Page
+
+      <Button onClick={onSubmit}>
+       {isLoading ? "Updating..." :"Click Me"}
+         </Button>
+    </div>
   )
 }
 
