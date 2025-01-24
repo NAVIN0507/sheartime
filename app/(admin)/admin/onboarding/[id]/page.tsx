89ea236dd8a1e32983b712f0ac69cfd62c2c6e66
NAVIN0507 <navinofficial2005@gmail.com>
2025-01-24 10:10:52 +0530

    Added Upload Thing

--- app/(admin)/admin/onboarding/[id]/page.tsx
+++ app/(admin)/admin/onboarding/[id]/page.tsx
@@ -3,12 +3,14 @@ import { Button } from '@/components/ui/button'
 import { db } from '@/database/drizzle'
 import { users } from '@/database/schema'
 import { getUserById } from '@/lib/actions/user.action'
+import { UploadButton , UploadDropzone } from '@/lib/utils/uploadthing'
 import { eq } from 'drizzle-orm'
 import { redirect, useRouter } from 'next/navigation'
 import React, { useState }  from 'react'
-
+import Image from 'next/image'
 const page = ({params} :{params:{id:string}}) => {
 const [isLoading, setisLoading] = useState(false);
+const [imageUrl, setimageUrl] = useState<string>('')
 const router = useRouter();
 //TODO: Imaplementing routing logic and anther
   const onSubmit = async () => {
@@ -29,7 +31,23 @@ const router = useRouter();
     }
   }
   return (
-    <div>Onboarding Page
+    <div className='mx-auto items-center justify-center'>Onboarding Page
+<UploadButton
+endpoint="imageUploader"
+onClientUploadComplete={(res)=>{
+  console.log('Files: ' , res);
+  setimageUrl(res[0].url)
+}}
+onUploadError={(error :Error)=>{
+  alert(`Error! ${error.message}`)
+}}
+className='bg-secondry-1 text-primary-1'
+/>
+{imageUrl.length ? (<>
+<div>
+  <Image src={imageUrl} alt='img'  width={500} height={300} />
+</div>
+</>) :''}
 
       <Button onClick={onSubmit}>
        {isLoading ? "Updating..." :"Click Me"}
