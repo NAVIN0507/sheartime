39394f9c88fe33a0c9b2dc792bdd1361a8d0d539
NAVIN0507 <navinofficial2005@gmail.com>
2025-02-21 17:52:02 +0530

    Changing Admin side bar

--- components/admin/LeftSideBar.tsx
+++ components/admin/LeftSideBar.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import { CalendarSync } from 'lucide-react'
 
 const LeftSideBar = () => {
   return (
     <section className='min-h-screen w-[300px] bg-primary-1 top-0 fixed'>
         <div className='flex flex-col'>
-            <Link href={"/"} className='flex flex-row gap-1 mt-5 border-b-2'>
+            <Link href={"/"} className='flex flex-row -ml-1 mt-5 border-b-2'>
             <Image
             src="/icons/logo-white.svg"
             alt='logo'
@@ -17,28 +18,27 @@ const LeftSideBar = () => {
             />
             <h1 className='my-auto text-3xl'>SHEARTIME</h1>
             </Link>
-            <div className='my-auto mt-32'>
+            <div className='my-auto mt-20 ml-2'>
             <div className='flex flex-col gap-10'>
-              
-                {AdminSideBar.map((link)=>{
-                    
-                    return(
-                        <Link href={link.route} key={link.route}>
-                        <div className='flex flex-row gap-2 w-[300px] mx-auto h-[50px] ' >
-                            <div className='my-auto ml-5 flex flex-row gap-3'>
-                          <Image
-                            src={link.img}
-                            alt={link.name}
-                            width={25}
-                            height={25}
-                            />
-                            <h3 className='text-[20px] font-light'>{link.name}</h3>
-                            </div>
-                           
-                        </div>
-                        </Link>
-                    )
-                })}
+              <div className=''>
+                <h1 className='text-2xl ml-2'>Bookings</h1>
+                <div className='flex flex-col gap-2 ml-7 mt-5'>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1'><CalendarSync />Today's Bookings</Button>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1 ml-3'><CalendarSync />Confirmed Bookings</Button>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1'><CalendarSync />Pending Bookings</Button>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1 ml-1'><CalendarSync />Cancelled Bookings</Button>
+                </div>
+                
+              </div>
+              <div className=''>
+                <h1 className='text-2xl ml-2'>Customers</h1>
+                <div className='flex flex-col gap-2 ml-7 mt-5'>
+                  <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1'><CalendarSync />Today's Bookings</Button>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1 ml-3'><CalendarSync />Confirmed Bookings</Button>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1'><CalendarSync />Pending Bookings</Button>
+                <Button className='shadow-none w-[180px] h-[40px] text-1xl hover:bg-secondry-1 hover:ease-in-out hover:text-primary-1 ml-1'><CalendarSync />Cancelled Bookings</Button>
+                </div>
+              </div>
             </div>
             </div>
         </div>
