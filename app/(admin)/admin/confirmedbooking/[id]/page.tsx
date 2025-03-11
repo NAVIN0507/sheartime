7b85766297ea31c5aee0f76373641b06a4160d22
NAVIN N <146793816+NAVIN0507@users.noreply.github.com>
2025-03-11 21:13:42 +0530

    Update page.tsx

--- app/(admin)/admin/confirmedbooking/[id]/page.tsx
+++ app/(admin)/admin/confirmedbooking/[id]/page.tsx
@@ -1,18 +1,23 @@
 import Bookings from '@/components/admin/Bookings';
-import { getBookingByType, getShopId } from '@/lib/actions/admin.action'
+import {confirmedBooking ,  getBookingByType, getShopId } from '@/lib/actions/admin.action'
 import { getUserById } from '@/lib/actions/user.action';
 import { redirect } from 'next/navigation';
 import React from 'react'
-
+import { CircleX } from 'lucide-react';
+import BookingCountCard from '@/components/admin/BookingCountCard';
 const page = async({params}:{params :{id:string}}) => {
     const shopId = await getShopId(params.id);
     if(!shopId) return redirect("/sign-in");
     const bookings = await getBookingByType({shopId:shopId , type:'BOOKED'})
     if(!bookings) return null;
+    const bookingCount = await confirmedBooking(shopId);
   return (
    <section className='-mt-16 ml-12'>
     <div>
+        <div className="justify-between items-center flex">
         <h1 className='font-mono text-3xl p-4'>Confirmed Bookings</h1>
+            <BookingCountCard type='confirm' typeName='confirm' count={bookingcount} icon={<CircleX />} className='bg-red-200' />
+            </div>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-10'>
       {bookings.map(async(booking)=>{
                 const user = await getUserById(booking.userId);
