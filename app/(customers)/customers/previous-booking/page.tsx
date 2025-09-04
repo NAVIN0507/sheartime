84ef9791a3b16b0cf7aba5af1e03cdde82fe9035
NAVIN N <146793816+NAVIN0507@users.noreply.github.com>
2025-09-04 19:31:55 +0530

    Update page.tsx

--- app/(customers)/customers/previous-booking/page.tsx
+++ app/(customers)/customers/previous-booking/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth'
-
+//this is previous booking page
 import { deleteBookingById, getAllBookingByUserId, getBookingByUserId, getShopsByShopId } from '@/lib/actions/user.action';
 import { formatDateTime } from '@/lib/utils';
 import React from 'react'
